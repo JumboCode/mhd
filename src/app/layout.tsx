d3a131fa3bb863995c1396b13f7423898b949a06
shaynesidman <147111519+shaynesidman@users.noreply.github.com>
2025-09-29 17:17:13 -0400

    changed 2 spacing to 4 spacing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,20 +2,20 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "MHD",
-  description: "MHD",
+    title: "MHD",
+    description: "MHD",
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body>
-        {children}
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <body>
+                {children}
+            </body>
+        </html>
+    );
 }
