4bf52be73f2d7ca6b9a9b42452535ca221c72f6d
shaynesidman <147111519+shaynesidman@users.noreply.github.com>
2025-10-05 11:37:09 -0400

    added eslint and basic rules

---
+++ eslint.config.mjs
@@ -0,0 +1,46 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+import pluginNext from '@next/eslint-plugin-next';
+
+export default [
+    js.configs.recommended,
+    ...tseslint.configs.recommended,
+    pluginReact.configs.flat.recommended,
+    {
+        files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+        languageOptions: { 
+            globals: {
+                ...globals.browser, 
+                ...globals.node
+            }
+        }
+    },
+    {
+        plugins: {
+            '@next/next': pluginNext
+        },
+        rules: {
+            ...pluginNext.configs.recommended.rules,
+            ...pluginNext.configs['core-web-vitals'].rules,
+        }
+    },
+    {
+        "settings": {
+            "react": {
+                "version": "detect"
+            }
+        }
+    },
+    {
+        rules: {
+            "no-console": "warn",
+            "no-var": "error",
+            "semi": ["warn", "always"],
+            "eqeqeq": ["error", "always"],
+            "@next/next/no-img-element": "warn",
+            "react/react-in-jsx-scope": "off",
+        },
+    },
+];
