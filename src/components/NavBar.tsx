40408a74a19fc4b3aa9a92c2cdea1491bb80aaf6
shaynesidman <147111519+shaynesidman@users.noreply.github.com>
2025-10-11 20:52:08 -0400

    changed navbar to use navbaritem for an example of passing props to react component

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,41 +1,16 @@
 "use client";
 
-import Link from "next/link";
+import NavBarItem from "./NavBarItem";
 
 export default function NavBar() {
     return (
         <nav className="max-w-3xl">
             <div className="flex flex-row justify-center items-center mt-2 mx-4 bg-alt w-full rounded-3xl">
-                <Link
-                    href="/button"
-                    className="py-1 px-2 hover:bg-second-alt transition duration-300"
-                >
-                    Button
-                </Link>
-                <Link
-                    href="/checkbox"
-                    className="py-1 px-2 hover:bg-second-alt transition duration-300"
-                >
-                    Checkbox
-                </Link>
-                <Link
-                    href="/input"
-                    className="py-1 px-2 hover:bg-second-alt transition duration-300"
-                >
-                    Input
-                </Link>
-                <Link
-                    href="/slider"
-                    className="py-1 px-2 hover:bg-second-alt transition duration-300"
-                >
-                    Slider
-                </Link>
-                <Link
-                    href="/tabs"
-                    className="py-1 px-2 hover:bg-second-alt transition duration-300"
-                >
-                    Tabs
-                </Link>
+                <NavBarItem text="button" />
+                <NavBarItem text="checkbox" />
+                <NavBarItem text="input" />
+                <NavBarItem text="slider" />
+                <NavBarItem text="tabs" />
             </div>
         </nav>
     );
--- src/components/NavBarItem.tsx
+++ src/components/NavBarItem.tsx
@@ -1,3 +1,16 @@
-export default function NavBarItem() {
-    return <div></div>;
+import Link from "next/link";
+
+type NavBarItemProps = {
+    text: string;
+};
+
+export default function NavBarItem({ text }: NavBarItemProps) {
+    return (
+        <Link
+            href={`${text}`}
+            className="py-1 px-2 hover:bg-second-alt transition duration-300"
+        >
+            {text}
+        </Link>
+    );
 }
