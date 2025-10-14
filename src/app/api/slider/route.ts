f9f6be634f82dc396926a29770938bbde18266e3
shaynesidman <147111519+shaynesidman@users.noreply.github.com>
2025-10-13 23:43:40 -0400

    finished slider api route

--- src/app/api/slider/route.ts
+++ src/app/api/slider/route.ts
@@ -1,4 +1,52 @@
-export async function GET() {
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(req: NextRequest) {
     try {
-    } catch (error) {}
+        const { searchParams } = new URL(req.url);
+        const characterIDParam = searchParams.get("characterID");
+
+        // Character ID must be included
+        if (characterIDParam === null) {
+            return NextResponse.json(
+                { error: "Character ID is necessary" },
+                { status: 400 },
+            );
+        }
+
+        const characterID = parseInt(characterIDParam, 10);
+
+        // Character ID must be a number between 1 and 84
+        if (isNaN(characterID) || characterID < 1 || characterID > 84) {
+            return NextResponse.json(
+                { error: "Character ID must be a number between 1 and 84" },
+                { status: 400 },
+            );
+        }
+
+        const res = await fetch(`https://swapi.dev/api/people/${characterID}/`);
+
+        if (!res.ok) {
+            if (res.status === 404) {
+                return NextResponse.json(
+                    { error: `Character ${characterID} not found` },
+                    { status: 404 },
+                );
+            }
+
+            return NextResponse.json(
+                { error: `Failed to fetch character data` },
+                { status: 502 }, // Bad gateway
+            );
+        }
+
+        const characterData = await res.json();
+
+        return NextResponse.json(characterData, { status: 200 });
+    } catch (error) {
+        console.error("Error fetching character:", error);
+        return NextResponse.json(
+            { error: "Internal server error" },
+            { status: 500 },
+        );
+    }
 }
