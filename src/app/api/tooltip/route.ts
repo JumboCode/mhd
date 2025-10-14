39a9f916c1595635fabffeb9199ab0db9d4736c6
shaynesidman <147111519+shaynesidman@users.noreply.github.com>
2025-10-14 12:37:29 -0400

    finished tooltip

--- src/app/api/tooltip/route.ts
+++ src/app/api/tooltip/route.ts
@@ -1,4 +1,30 @@
+import { NextResponse } from "next/server";
+
 export async function GET() {
     try {
-    } catch (error) {}
+        const res = await fetch("https://thequoteshub.com/api/random");
+
+        if (!res.ok) {
+            if (res.status === 404) {
+                return NextResponse.json(
+                    { error: "No quote data found" },
+                    { status: 404 },
+                );
+            }
+
+            return NextResponse.json(
+                { error: "Failed to fetch any quote data" },
+                { status: 502 }, // Bad gateway
+            );
+        }
+
+        const data = await res.json();
+
+        return NextResponse.json(data, { status: 200 });
+    } catch (error) {
+        return NextResponse.json(
+            { error: "Internal server error" },
+            { status: 500 },
+        );
+    }
 }
