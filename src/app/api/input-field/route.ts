44fce7c0450800b7b6c66383bda949fe4ee18f49
shaynesidman <147111519+shaynesidman@users.noreply.github.com>
2025-10-14 13:12:39 -0400

    finished input field backend

--- src/app/api/input-field/route.ts
+++ src/app/api/input-field/route.ts
@@ -3,16 +3,53 @@ import { NextRequest, NextResponse } from "next/server";
 export async function GET(req: NextRequest) {
     try {
         const { searchParams } = new URL(req.url);
-        const input = searchParams.get("input");
+        const inputNumberParam = searchParams.get("input");
 
-        // Missing query string
-        if (input == null) {
+        // Input number must be included
+        if (inputNumberParam == null) {
             return NextResponse.json(
-                { error: "Missing search parameters" },
+                { error: "Missing input number" },
                 { status: 400 },
             );
         }
 
-        // reverse input
-    } catch (error) {}
+        const inputNumber = parseInt(inputNumberParam, 10);
+
+        // Input number must be a number
+        if (isNaN(inputNumber) || inputNumber < 9 || inputNumber > 32) {
+            return NextResponse.json(
+                { error: "Input number must be a number between 9 and 32" },
+                { status: 400 },
+            );
+        }
+
+        const res = await fetch(
+            `https://opentdb.com/api.php?amount=1&category=${inputNumber}`,
+        );
+
+        if (!res.ok) {
+            if (res.status === 404) {
+                return NextResponse.json(
+                    {
+                        error: `Endpoint for input number ${inputNumber} not found`,
+                    },
+                    { status: 404 },
+                );
+            }
+
+            return NextResponse.json(
+                { error: `Failed to get input number data` },
+                { status: 502 }, // Bad gateway
+            );
+        }
+
+        const triviaQuestion = await res.json();
+
+        return NextResponse.json(triviaQuestion, { status: 200 });
+    } catch (error) {
+        return NextResponse.json(
+            { error: "Internal server error" },
+            { status: 500 },
+        );
+    }
 }
