80253ab6470ffe1fe087f35575049c61cc9ffbe5
woleary2 <will.oleary217@gmail.com>
2025-10-26 12:58:56 -0400

    added number to end of slider

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -25,20 +25,24 @@ export default function Slider({ min, max }: SliderProps) {
                 <h1 className="text-xl font-bold mb-2">
                     Drag and slide to reveal new characters
                 </h1>
-                <input
-                    min={min}
-                    max={max}
-                    type="range"
-                    onChange={(e) => setValue(Number(e.target.value))}
-                    className="w-80 h-2 appearance-none rounded-lg cursor-pointer accent-gray-700 mb-5"
-                    style={{
-                        background: `linear-gradient(to right, 
-                    rgb(45, 46, 48) 0%, 
-                    rgb(45, 46, 48) ${fillPercent}%, 
-                    rgb(209,213,219) ${fillPercent}%, 
-                    rgb(209,213,219) 100%)`,
-                    }}
-                />
+                <div className="flex">
+                    <input
+                        min={min}
+                        max={max}
+                        type="range"
+                        onChange={(e) => setValue(Number(e.target.value))}
+                        className="w-80 h-2 appearance-none rounded-lg cursor-pointer accent-gray-700 mb-5"
+                        style={{
+                            background: `linear-gradient(to right, 
+                        rgb(45, 46, 48) 0%, 
+                        rgb(45, 46, 48) ${fillPercent}%, 
+                        rgb(209,213,219) ${fillPercent}%, 
+                        rgb(209,213,219) 100%)`,
+                        }}
+                    />
+                    <p className="ml-5">{value}</p>
+                </div>
+
                 <button
                     onClick={fetchFromApi}
                     className="rounded-lg h-12 w-40 bg-sky-900 m-2 flex items-center justify-center text-white text-lg m-4"
