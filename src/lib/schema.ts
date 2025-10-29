182c838c5568133655a28ecc82763667ca576e44
shaynesidman <147111519+shaynesidman@users.noreply.github.com>
2025-10-29 01:49:26 -0400

    added drizzle, created schema, initialized db

---
+++ src/lib/schema.ts
@@ -0,0 +1,76 @@
+import { pgTable, integer, text, serial, boolean } from "drizzle-orm/pg-core";
+
+// Represents metadata (relatively unchanging data) about a school
+export const schools = pgTable("schools", {
+    id: serial("id").primaryKey(),
+    name: text("name").notNull(),
+    town: text("town").notNull(),
+});
+
+// Ties a school to the years it has participated
+export const yearlySchoolParticipation = pgTable(
+    "yearly_school_participation",
+    {
+        id: serial("id").primaryKey(),
+        schoolId: integer("school_id")
+            .notNull()
+            .references(() => schools.id),
+        year: integer("year").notNull(),
+    },
+);
+
+// Represents a teacher and their relevant information
+export const teachers = pgTable("teachers", {
+    id: serial("id").primaryKey(),
+    firstName: text("first_name").notNull(),
+    lastName: text("last_name").notNull(),
+    email: text("email").notNull(),
+});
+
+// Represents a project and its relevant information
+export const projects = pgTable("projects", {
+    id: serial("id").primaryKey(),
+    schoolId: integer("school_id")
+        .notNull()
+        .references(() => schools.id),
+    teacherId: integer("teacher_id")
+        .notNull()
+        .references(() => teachers.id),
+    entryId: integer("entry_id").unique(),
+    title: text("title").notNull(),
+    division: text("division").notNull(),
+    category: text("category").notNull(),
+    year: integer("year").notNull(),
+    group: boolean("group").notNull(), // True if group project, false if individual
+});
+
+// Ties a student to a project and a school and whether they are returning
+export const students = pgTable("students", {
+    id: serial("id").primaryKey(),
+    projectId: integer("project_id")
+        .notNull()
+        .references(() => projects.id),
+    schoolId: integer("school_id")
+        .notNull()
+        .references(() => schools.id),
+});
+
+// Ties a teacher to the years they participated
+export const yearlyTeacherData = pgTable("yearly_teacher_data", {
+    id: serial("id").primaryKey(),
+    teacherId: integer("teacher_id")
+        .notNull()
+        .references(() => teachers.id),
+    year: integer("year").notNull(),
+});
+
+// Ties a teacher to a school
+export const teacherSchools = pgTable("teacher_schools", {
+    id: serial("id").primaryKey(),
+    teacherId: integer("teacher_id")
+        .notNull()
+        .references(() => teachers.id),
+    schoolId: integer("school_id")
+        .notNull()
+        .references(() => schools.id),
+});
