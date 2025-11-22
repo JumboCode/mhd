3189de0515a5a393649f3260bee09eca9bd70789
stevenvb101 <stevenvb101@gmail.com>
2025-11-22 13:16:27 -0500

    Started work on bargraph

---
+++ src/components/Bargraph.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Bargraph from "@/components/Bargraph";
+
+export default function DashboardPage() {
+    // return <Bargraph />;
+    return (
+        <div className="min-h-screen flex justify-center items-center">
+            <Bargraph />
+        </div>
+    );
+}
