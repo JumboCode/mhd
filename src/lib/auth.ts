18b3c193283052152f881b28c2ee70a7d2ff8816
Jack-86 <jack.l008600@gmail.com>
2025-12-22 07:39:07 +0800

    Add premilinary auth and form logic

---
+++ src/lib/auth.ts
@@ -0,0 +1,18 @@
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { db } from "./db";
+import { schema } from "./schema";
+import { nextCookies } from "better-auth/next-js";
+
+export const auth = betterAuth({
+    emailAndPassword: {
+        enabled: true,
+    },
+
+    database: drizzleAdapter(db, {
+        provider: "pg",
+        schema,
+    }),
+
+    plugins: [nextCookies()],
+});
