979932e0d063b8867d954e08bb3fbb59d308bb9b
shaynesidman <147111519+shaynesidman@users.noreply.github.com>
2026-01-04 11:24:04 -0500

    migrated tables from auth to dummy_data branch

--- src/lib/schema.ts
+++ src/lib/schema.ts
@@ -98,16 +98,15 @@ export const session = pgTable(
     {
         id: text("id").primaryKey(),
         expiresAt: timestamp("expires_at").notNull(),
-        token: text("token").notNull().unique(),
-        createdAt: timestamp("created_at").defaultNow().notNull(),
-        updatedAt: timestamp("updated_at")
-            .$onUpdate(() => /* @__PURE__ */ new Date())
-            .notNull(),
         ipAddress: text("ip_address"),
         userAgent: text("user_agent"),
         userId: text("user_id")
             .notNull()
             .references(() => user.id, { onDelete: "cascade" }),
+        createdAt: timestamp("created_at").defaultNow().notNull(),
+        updatedAt: timestamp("updated_at")
+            .$onUpdate(() => /* @__PURE__ */ new Date())
+            .notNull(),
     },
     (table) => [index("session_userId_idx").on(table.userId)],
 );
