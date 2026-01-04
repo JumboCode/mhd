a13b545e34917a475d0a608a0ce7f234c7974b76
shaynesidman <147111519+shaynesidman@users.noreply.github.com>
2026-01-04 12:19:25 -0500

    created canvas element for the cool animated login page

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,10 +1,17 @@
 import AuthForm from "@/components/AuthForm";
+import WarpShader from "@/components/WarpShader";
 
 export default function SignInPage() {
     return (
-        <div className="bg-muted flex min-h-svh flex-col items-center justify-center p-6 md:p-10">
-            <div className="w-full max-w-sm md:max-w-4xl">
+        <div className="flex h-screen flex-col items-center justify-center">
+            <div className="w-full h-full flex flex-row items-center">
                 <AuthForm />
+                <div className="w-1/2 h-full">
+                    <WarpShader
+                        colorFront={{ r: 0.784, g: 0.192, b: 0.22, a: 1 }}
+                        colorBack={{ r: 0, g: 0, b: 0, a: 1 }}
+                    />
+                </div>
             </div>
         </div>
     );
