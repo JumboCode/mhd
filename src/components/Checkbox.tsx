2906e85fe8f7a90350442326bbbbd0f25ffca036
Dan Glorioso <125602370+danglorioso@users.noreply.github.com>
2026-01-19 13:34:58 -0500

    Center X in checkbox

--- src/components/Checkbox.tsx
+++ src/components/Checkbox.tsx
@@ -21,7 +21,7 @@ function Checkbox({
         >
             <CheckboxPrimitive.Indicator
                 data-slot="checkbox-indicator"
-                className="grid place-content-center text-current transition-none"
+                className="flex items-center justify-center w-full h-full text-current transition-none"
             >
                 <X className="size-3.5" />
             </CheckboxPrimitive.Indicator>
