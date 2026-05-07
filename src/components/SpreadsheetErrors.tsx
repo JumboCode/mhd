18c1bdb2d766fe1de056cf6a7127e69826321bdc
shaynesidman <147111519+shaynesidman@users.noreply.github.com>
2026-05-07 12:34:11 -0400

    chore: remove unused files w/ knip

--- src/components/SpreadsheetErrors.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-export default function SpreadsheetErrors() {
-    return (
-        <div className="border rounded-lg w-full h-32">
-            <p>Hello</p>
-        </div>
-    );
-}
